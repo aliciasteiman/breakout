0edf748dde0de09dfde590608814911a53ef8f95
Suomo Ammah <sna19@duke.edu>
2020-02-10 02:52:27 -0500

    writing powerups

--- src/breakout/PowerUp.java
+++ src/breakout/PowerUp.java
@@ -1,7 +1,26 @@
 package breakout;
+import java.util.*;
+import javafx.scene.*;
+import javafx.scene.image.*;
+
 
 public class PowerUp extends Game{
+    private ArrayList<PowerUp> myPowerups;
+
+    
+
     public void createpowerup(){
 
+    }
+
+    public String setPower(){
+        ArrayList<String> listpowers = new ArrayList<>();
+        listpowers.add("longerpaddle");
+        listpowers.add("shorterpaddle");
+        listpowers.add("multipleballs");
+        listpowers.add("ballspeedup");
+        listpowers.add("ballslowdown");
+        Collections.shuffle(listpowers);
+       return listpowers.get(0);
     }
 }
